90b2a02cf59c9a498f3ad8a2a14ee45098a79025
vijayveer <vijayvir@imyourdoc.com>
2016-02-02 16:27:23 +0530

    Intial commits
    Intial commits

---
+++ NavigationAnimaion/ViewController.m
@@ -0,0 +1,67 @@
+//
+//  ViewController.m
+//  NavigationAnimaion
+//
+//  Created by OSX on 29/01/16.
+//  Copyright (c) 2016 Vijayvir. All rights reserved.
+//
+
+#import "ViewController.h"
+#import  "SecondViewController.h"
+
+@interface ViewController ()<UINavigationControllerDelegate>
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationController.delegate = self;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - UINavigationControllerDelegate
+
+- (id <UIViewControllerAnimatedTransitioning>)navigationController:(UINavigationController *)navigationController
+                                   animationControllerForOperation:(UINavigationControllerOperation)operation
+                                                fromViewController:(UIViewController *)fromVC
+                                                  toViewController:(UIViewController *)toVC
+{
+    
+    if (([toVC isKindOfClass:[SecondViewController class]] &&  [fromVC isKindOfClass:[ViewController class]]) || ([toVC isKindOfClass:[ViewController class]] &&  [fromVC isKindOfClass:[SecondViewController class]] ) )
+    {
+        switch(operation)
+        {
+            case UINavigationControllerOperationPush:
+                return [[Animator alloc] init];
+            case UINavigationControllerOperationPop:
+                return [[Animator alloc] init];
+            default:
+                return nil;
+        }
+    }
+    
+ 
+    
+    return nil;
+}
+@end
